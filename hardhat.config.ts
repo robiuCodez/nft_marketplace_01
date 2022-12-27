79568e990de0408fefd19e50c0dac570a34457b8
robiuCodez <muhdrobiu777@gmail.com>
2022-12-27 19:53:59 +0100

    smart contract deployed to network --localhost

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,24 @@
-import { HardhatUserConfig } from "hardhat/config";
+import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    rinkeby: {
+      url: process.env.ENDPOINT_URL as string,
+      accounts: [process.env.DEPLOYER_KEY as string],
+    },
+  },
+  // paths: {
+  //   sources: "./src/contracts",
+  //   artifacts: "./src/abis",
+  // },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,30 @@
 import { ethers } from "hardhat";
+import fs from "fs";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // 12D3KooWA9CrrGA7iVxC57fWZEAHifAWL2gm3uq8ZQWcuHh7qBnu
+  const base_uri =
+    "https://ipfs.io/ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc";
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const DevConnect = await ethers.getContractFactory("DevConnect");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const devConnect = await DevConnect.deploy("DevConnect", "DEV", base_uri);
 
-  await lock.deployed();
+  // ensure this contract is deployed
+  await devConnect.deployed();
+  // get address of deployed contract on the internet
+  const address = JSON.stringify({ address: devConnect.address }, null, 4);
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  // save address to file
+  fs.writeFile("./abis/contractAddress.json", address, "utf8", (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+
+  console.log("Deployed Contract Address : ", devConnect.address);
+  // 0x5FbDB2315678afecb367f032d93F642f64180aa3
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -21,3 +33,13 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// Account #0: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 (10000 ETH)
+// Private Key: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+// Account #1: 0x70997970C51812dc3A010C7d01b50e0d17dc79C8 (10000 ETH)
+// Private Key: 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d
+
+// Account #2: 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC (10000 ETH)
+// Private Key: 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a
